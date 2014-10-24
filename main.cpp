25a0dca8ccd6bb191c18a046a1fa9f4073958059
Alex Opryshko <alexopryshko@yandex.ru>
2014-10-25 00:06:12 +0400

    move elements to for

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std::chrono;
 #define ARRAY_SIZE 100
 #define MIN_SHIFT 10
 #define MAX_SHIFT 24
-#define NUMBER_OF_TESTS 16
+#define NUMBER_OF_TESTS 128
 #define NUMBER_OF_TESTS_IN_ONE_ITERATION 2048
 #define NUMBER_OF_STEPS 256
 
@@ -57,7 +57,8 @@ void create_random_sequence(vector<el<N>> &elements) {
 }
 
 template <size_t N>
-double test_cache(el<N> *element) {
+double test_cache(el<N> *first_element) {
+    volatile auto element = first_element;
     long t_start = duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count();
     for (int i = 0; i < NUMBER_OF_TESTS_IN_ONE_ITERATION; i++) {
         DUPLICATION(
@@ -68,12 +69,11 @@ double test_cache(el<N> *element) {
                                                 DUPLICATION(
                                                         DUPLICATION(
                                                                 DUPLICATION(
-                                element->next = element;
+                                element = element->next;
         ))))))))
     }
     long t_stop = duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count();
     double duration = double(t_stop - t_start) / (NUMBER_OF_STEPS * NUMBER_OF_TESTS_IN_ONE_ITERATION);
-
     return duration;
 }
 
@@ -81,18 +81,17 @@ template <size_t N>
 void test() {
     for (unsigned i = MIN_SHIFT; i <= MAX_SHIFT; i++) {
         size_t el_size = sizeof(el<N>);
-        unsigned long array_size = ((unsigned long) 1 << i);
-        vector<el<N>> elements(array_size);
+        unsigned long array_size = ((unsigned long) 1 << i) / el_size;
         double linear_sum_time = 0;
         double random_sum_time = 0;
-        create_linear_sequence(elements);
-
 
         for (int j = 0; j < NUMBER_OF_TESTS; j++) {
+            vector<el<N>> elements(array_size);
+            create_linear_sequence(elements);
             linear_sum_time += test_cache(&elements[0]);
             //
-            //create_random_sequence(elements);
-            //random_sum_time += test_cache(&elements[0]);
+            create_random_sequence(elements);
+            random_sum_time += test_cache(&elements[0]);
         }
         cout << linear_sum_time / double(NUMBER_OF_TESTS) << " : ";
         cout << random_sum_time / double(NUMBER_OF_TESTS) << "\n";
